817c02cc859c108add17aac5ef032bf91437b2b3
Eugene MechanisM <eugene@mechanism.name>
2015-04-21 11:14:58 +0300

    Removed signals just yet

--- django_uwsgi/emperor/apps.py
+++ django_uwsgi/emperor/apps.py
@@ -5,6 +5,3 @@ class EmperorConfig(AppConfig):
     name = 'django_uwsgi.emperor'
     label = 'uwsgi_emperor'
     verbose_name = 'uWSGI Emperor'
-
-    def ready(self):
-        pass# import django_uwsgi.emperor.signals

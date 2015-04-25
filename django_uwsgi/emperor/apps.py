349eeb3cff83fefef03369882f04038935506091
Eugene MechanisM <eugene@mechanism.name>
2015-04-25 13:56:47 +0300

    Changed label for Emperor

--- django_uwsgi/emperor/apps.py
+++ django_uwsgi/emperor/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class EmperorConfig(AppConfig):
     name = 'django_uwsgi.emperor'
-    label = 'uwsgi_emperor'
+    label = 'emperor'
     verbose_name = 'uWSGI Emperor'

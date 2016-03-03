d594b5a590a8ba47f18b0fcc32c6ef68b3fa34be
Martins Rudens <martins.rudens@gmail.com>
2016-03-03 13:06:07 +0200

    Resolve compatibility issue: django.conf.urls.patterns() is deprecated and will be removed in Django 1.10

--- django_uwsgi/urls.py
+++ django_uwsgi/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.UwsgiStatus.as_view(), name='uwsgi_index'),
     url(r'^reload/$', views.UwsgiReload.as_view(), name='uwsgi_reload'),
     url(r'^clear_cache/$', views.UwsgiCacheClear.as_view(), name='uwsgi_cache_clear'),
-)
+]
